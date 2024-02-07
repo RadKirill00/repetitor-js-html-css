1ee36493e1430cb8463a4f05a7e89b86206ce53c
RadKirill <radkirill00@mail.ru>
2024-02-07 22:24:47 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-let p = document.querySelector("#elem");
-let btn = document.querySelector("#button");
-let btn2 = document.querySelector("#button2");
 
-function func1() {
-  p.textContent = Number(p.textContent) + 1
-}
-
-function func2() {
-  p.textContent = Number(p.textContent) - 1;
-}
-
-btn.addEventListener("click", func1);
-btn2.addEventListener('click', func2)
