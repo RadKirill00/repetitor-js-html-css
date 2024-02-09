f5c16cd3b237c3c08eeb4a193e9a964376e8a8b2
RadKirill <radkirill00@mail.ru>
2024-02-09 18:13:52 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let elems = document.querySelectorAll("ul li");
+
+for (let elem of elems) {
+  elem.addEventListener('click', function func() {
+    if (this.textContent < 10) {
+      this.textContent = Number(this.textContent) + 1;
+    } else {
+      this.removeEventListener("click", func);
+    }
+  })
+}
 
